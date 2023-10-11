71dcc6676f85ad05d7bcfd744b6333d176ba301a
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-11 16:03:49 +0200

    feature h. responsive

--- src/app/components/Features.tsx
+++ src/app/components/Features.tsx
@@ -1,7 +1,7 @@
 const Features = () => {
   return (
-    <div className="flex flex-col py-32 sm:py-48 gap-12 sm:gap-28 items-center w-full text-center">
-      <h2 className="text-5xl sm:text-7xl font-averia font-bold tracking-wide pb-16">
+    <div className="flex flex-col py-32 lg:py-48 gap-12 sm:gap-16 lg:gap-28 items-center w-full text-center">
+      <h2 className="text-5xl lg:text-7xl font-averia font-bold tracking-wide pb-16">
         How it works?
       </h2>
 
@@ -11,17 +11,17 @@ const Features = () => {
             <img
               src="/irregular-circle.png"
               alt="irregular circle"
-              className="z-0 relative w-16 sm:w-36"
+              className="z-0 relative w-16 sm:w-28 lg:w-36"
             />
-            <h2 className="z-10 absolute text-3xl sm:text-6xl font-semibold font-averia text-white">
+            <h2 className="z-10 absolute text-3xl sm:text-4xl lg:text-6xl font-semibold font-averia text-white">
               01
             </h2>
           </div>
-          <div className="w-[110px] sm:w-[255px] flex flex-col gap-2">
-            <h3 className="font-averia text-lg sm:text-3xl font-semibold tracking-wider">
+          <div className="w-[110px] sm:w-[200px] lg:w-[255px] flex flex-col gap-2">
+            <h3 className="font-averia text-lg sm:text-2xl lg:text-3xl font-semibold tracking-wider">
               Browse
             </h3>
-            <p className="font-averia text-[10px] sm:text-[17px] font-semibold text-dark">
+            <p className="font-averia text-[10px] sm:text-[14px] lg:text-[17px] font-semibold text-dark">
               Feel free to look around, explore the platform, and decide if you
               like what you see.
             </p>
@@ -31,7 +31,7 @@ const Features = () => {
         <img
           src="/up-lines.png"
           alt="up lines"
-          className="z-10 absolute left-20 sm:left-72 -top-[24px] sm:top-[-50px] w-[100px] sm:w-[380px]"
+          className="z-10 absolute left-20 sm:left-36 lg:left-72 -top-[24px] sm:-top-[65px] lg:top-[-50px] w-[100px] sm:w-[240px] lg:w-[380px]"
         />
 
         <div className="flex flex-col gap-10 sm:gap-16">
@@ -39,17 +39,17 @@ const Features = () => {
             <img
               src="/irregular-circle.png"
               alt="irregular circle"
-              className="z-0 relative w-16 sm:w-36"
+              className="z-0 relative w-16 sm:w-28 lg:w-36"
             />
-            <h2 className="z-10 absolute text-3xl sm:text-6xl font-semibold font-averia text-white">
+            <h2 className="z-10 absolute text-3xl sm:text-4xl lg:text-6xl font-semibold font-averia text-white">
               02
             </h2>
           </div>
-          <div className="w-[110px] sm:w-[255px] flex flex-col gap-2">
-            <h3 className="font-averia text-lg sm:text-3xl font-semibold tracking-wider">
+          <div className="w-[110px] sm:w-[200px] lg:w-[255px] flex flex-col gap-2">
+            <h3 className="font-averia text-lg sm:text-2xl lg:text-3xl font-semibold tracking-wider">
               Sign Up
             </h3>
-            <p className="font-averia text-[10px] sm:text-[17px] font-semibold text-dark">
+            <p className="font-averia text-[10px] sm:text-[14px] lg:text-[17px] font-semibold text-dark">
               Just a very simple sign-up, nothing out of the ordinary: an email
               and a password, that's it!
             </p>
@@ -59,7 +59,7 @@ const Features = () => {
         <img
           src="/down-lines.png"
           alt="down lines"
-          className="z-10 absolute right-20 sm:right-72 bottom-28 sm:bottom-32 w-[100px] sm:w-[380px]"
+          className="z-10 absolute right-20 sm:right-36 lg:right-72 bottom-28 lg:bottom-32 w-[100px] sm:w-[240px] lg:w-[380px]"
         />
 
         <div className="flex flex-col gap-10 sm:gap-16">
@@ -67,17 +67,17 @@ const Features = () => {
             <img
               src="/irregular-circle.png"
               alt="irregular circle"
-              className="z-0 relative w-16 sm:w-36"
+              className="z-0 relative w-16 sm:w-28 lg:w-36"
             />
-            <h2 className="z-10 absolute text-3xl sm:text-6xl font-semibold font-averia text-white">
+            <h2 className="z-10 absolute text-3xl sm:text-4xl lg:text-6xl font-semibold font-averia text-white">
               03
             </h2>
           </div>
-          <div className="w-[110px] sm:w-[255px] flex flex-col gap-2">
-            <h3 className="font-averia text-lg sm:text-3xl font-semibold tracking-wider">
+          <div className="w-[110px] sm:w-[200px] lg:w-[255px] flex flex-col gap-2">
+            <h3 className="font-averia text-lg sm:text-2xl lg:text-3xl font-semibold tracking-wider">
               Enjoy
             </h3>
-            <p className="font-averia text-[10px] sm:text-[17px] font-semibold text-dark">
+            <p className="font-averia text-[10px] sm:text-[14px] lg:text-[17px] font-semibold text-dark">
               Now you’ll have full access to our entire platform and we hope you
               benefit from it!
             </p>
@@ -85,7 +85,7 @@ const Features = () => {
         </div>
       </div>
 
-      <button className="border px-4 py-2 sm:py-4 text-lg sm:text-xl font-semibold font-averia rounded-full hover:shadow-lg shadow-black">
+      <button className="border px-4 py-2 lg:py-4 text-lg lg:text-xl font-semibold font-averia rounded-full hover:shadow-lg shadow-black">
         🥳 Sign me Up Already!
       </button>
     </div>
