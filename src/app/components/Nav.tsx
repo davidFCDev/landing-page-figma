6bc116931c0dd63294352f5b65c4fb24af7d73d2
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-12 14:27:38 +0200

    sign up button

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -50,7 +50,7 @@ const Nav = () => {
               transition={{ duration: 0.5 }}
               className="p-6 bg-dimWhite shadow-sm shadow-gray3 absolute top-14 right-0 mx-4 my-1 min-w-[100px] rounded-lg sidebar font-rubik"
             >
-              <ul className="list-none flex gap-3 justify-end items-start flex-1 flex-col tracking-wider">
+              <ul className="list-none flex gap-2 justify-end items-start flex-1 flex-col tracking-wider">
                 <Link href="#" className="hover:text-orange text-xs sm:text-lg">
                   Home
                 </Link>
@@ -72,6 +72,9 @@ const Nav = () => {
                 <Link href="#" className="hover:text-orange text-xs sm:text-lg">
                   Blog
                 </Link>
+                <button className="text-xs sm:text-lg text-orange active:text-black">
+                  Sign Up
+                </button>
               </ul>
             </motion.div>
           )}
